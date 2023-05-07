f8ba716531cf4c9ebc11375a2a8ec3f1dad15f8a
Ely-Dzn <75621116+Ely-Dzn@users.noreply.github.com>
2023-05-07 02:46:42 -0300

    Adicionar arquivos de projeto.

---
+++ MyProject.BLL/StatusRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Myproject.DAL.DBContext;
+using Myprojec.MODEL;
+
+namespace MyProject.BLL
+{
+    public class StatusRepository
+    {
+        public static void Add(Status status)
+        {
+            using (var dbContext = new CUsersCcasmSourceReposAtvlp3tabela2MyprojectDalDatabaseDatabase1MdfContext())
+            {
+                dbContext.Add(status);
+                dbContext.SaveChanges();
+            }
+        }
+    }
+}
---
+++ MyProject.MODEL/Status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Myprojec.MODEL;
+
+public partial class Status
+{
+    public int Id { get; set; }
+
+    public string? Status1 { get; set; }
+
+    public virtual ICollection<Projeto> Projetos { get; set; } = new List<Projeto>();
+}
